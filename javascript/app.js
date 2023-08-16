8557d100e6f73a0bc4743fd6e68fc46d355b3ec4
LeoPaiva <sebamza1@gmail.com>
2023-08-15 22:27:00 -0300

    correccion imagenes

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,3 @@
-
-
 class Producto {
     constructor(codigo, precio, marca, img) {
         this.codigo = codigo;
@@ -24,69 +22,116 @@ class Lista {
             tarjetaProducto.classList.add("producto");
             tarjetaProducto.innerHTML = `
                 <img src="${producto.img}">
+                <div class="detalle-producto">
                 <h2>Precio: ${producto.precio}</h2>
                 <p>Código: ${producto.codigo}</p>
                 <p>Marca: ${producto.marca}</p>
                 <button id="btn-${producto.codigo}"><img src="./img/Shopping-Cart.png"></button>
+                </div>
             `;
             contenedor.appendChild(tarjetaProducto);
         });
     }
 }
 
-const p1 = new Producto("M023", 32412, "RedDragon", "./img/productos/m01.png");
-const p2 = new Producto("M024", 42412, "BlueFalcon", "./img/productos/m02.png");
-const p3 = new Producto("M025", 52412, "GreenPhoenix", "./img/productos/m01.png");
-const p4 = new Producto("M026", 62412, "RedDragon", "./img/productos/t02.png");
-const p5 = new Producto("M027", 72412, "BlueFalcon", "./img/productos/a01.png");
-const p6 = new Producto("M028", 82412, "GreenPhoenix", "./img/productos/mic02.png");
+class Carrito {
+    constructor() {
+        this.productos = [];
+        this.carritoStorage = "carrito"; // Cambia esto a tu nombre de almacenamiento correcto
+    }
+
+    agregar(producto) {
+        let productosCarrito = JSON.parse(localStorage.getItem(this.carritoStorage));
+
+        if (!productosCarrito) {
+            producto.cantidad = 1;
+            localStorage.setItem(this.carritoStorage, JSON.stringify([producto]));
+            this.productos.push(producto);
+        } else {
+            let indice = productosCarrito.findIndex(pCarrito => pCarrito.codigo === producto.codigo);
+            const nuevoproductosCarrito = [...productosCarrito];
+
+            if (indice === -1) {
+                producto.cantidad = 1;
+                nuevoproductosCarrito.push(producto);
+            } else {
+                nuevoproductosCarrito[indice].cantidad++;
+            }
+            localStorage.setItem(this.carritoStorage, JSON.stringify(nuevoproductosCarrito));
+            this.productos = nuevoproductosCarrito;
+        }
+        toasty(producto);
+    }
+
+    mostrar() {
+        const carritoStorage = "carrito"; // Asegúrate de que coincida con el nombre usado para almacenar en localStorage
+        const productosCarrito = JSON.parse(localStorage.getItem(carritoStorage));
+
+        if (!productosCarrito || productosCarrito.length === 0) {
+            Swal.fire({
+                title: 'Carrito vacío',
+                text: 'No hay productos en el carrito',
+                icon: 'warning',
+                width: 250,
+                position: 'top-end',
+                confirmButtonText: 'Cerrar'
+            });
+            return;
+        }
+
+        const carritoHTML = productosCarrito.map(producto => `
+            <div class="producto-carrito">
+                <img src="${producto.img}" class="miniatura swal2-image">
+                <p>${producto.marca}</p>
+                <p>Precio: ${producto.precio}</p>
+                <button> - </button>     ${producto.cantidad}     <button> + </button>
+            </div>
+        `).join("");
+
+        Swal.fire({
+            title: 'Carrito',
+            html: carritoHTML,
+            position: 'top-end',
+            width: 250,
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            imageHeight: 50 // Define la altura de la imagen (opcional)
+        });
+    }
+}
 
 const listaProductos = new Lista();
+const carrito = new Carrito();
+
+const p1 = new Producto("M023", 32412, "RedDragon", "./img/productos/m01.png");
+const p2 = new Producto("M024", 42412, "BlueFalcon", "./img/productos/m02.png");
+const p3 = new Producto("M025", 52412, "GreenPhoenix", "./img/productos/mic01.png");
+const p4 = new Producto("M026", 62412, "RedDragon", "./img/productos/t02.png");
+const p5 = new Producto("M027", 72412, "BlueFalcon", "./img/productos/a01.png");
+const p6 = new Producto("M029", 82412, "GreenPhoenix", "./img/productos/p01.png");
+const p7 = new Producto("M030", 62412, "RedDragon", "./img/productos/a02.png");
+const p8 = new Producto("M031", 72412, "BlueFalcon", "./img/productos/t01.png");
+const p9 = new Producto("M032", 82412, "GreenPhoenix", "./img/productos/p02.png");
 
 listaProductos.agregar(p1);
 listaProductos.agregar(p2);
 listaProductos.agregar(p3);
 listaProductos.agregar(p4);
-listaProductos.agregar(p5);
+listaProductos.agregar(p5); 
 listaProductos.agregar(p6);
+listaProductos.agregar(p7);
+listaProductos.agregar(p8);
+listaProductos.agregar(p9);
 
 listaProductos.mostrarLista();
 
 listaProductos.items.forEach(producto => {
     const btn = document.getElementById(`btn-${producto.codigo}`);
-
-    btn.addEventListener("click", () => agregarCarrito(producto));
+    btn.addEventListener("click", () => carrito.agregar(producto));
 });
 
-function agregarCarrito(producto) {
-    console.log("*******" + producto.codigo);
-    let carritoStorage = "carrito"; // Cambia esto a tu nombre de almacenamiento correcto
-    let productosCarrito = JSON.parse(localStorage.getItem(carritoStorage));
-    console.log(productosCarrito);
-
-    if (!productosCarrito) {
-        console.log("carrito Vacio");
-        let nuevoProducto = producto;
-        nuevoProducto.cantidad = 1;
-        localStorage.setItem(carritoStorage, JSON.stringify([nuevoProducto]));
-        toasty(producto);
-    } else {
-        console.log("carrito con elementos");
-        let indice = productosCarrito.findIndex(pCarrito => pCarrito.codigo === producto.codigo);
-        console.log(indice);
-        const nuevoproductosCarrito = [...productosCarrito];
-
-        if (indice === -1) {
-            let nuevoProducto = producto;
-            nuevoProducto.cantidad = 1;
-            nuevoproductosCarrito.push(nuevoProducto);
-        } else {
-            nuevoproductosCarrito[indice].cantidad++;
-        }
-        localStorage.setItem(carritoStorage, JSON.stringify(nuevoproductosCarrito));
-        toasty(producto);
-    }
-}
+document.getElementById("mostrar-carrito-btn").addEventListener("click", () => carrito.mostrar());
 
 function toasty(producto) {
     Toastify({
@@ -97,51 +142,3 @@ function toasty(producto) {
           },
     }).showToast();
 }
-
-
-/**********************************************/
-
-document.getElementById("mostrar-carrito-btn").addEventListener("click", mostrarCarrito);
-
-function mostrarCarrito() {
-    const carritoStorage = "carrito"; // Asegúrate de que coincida con el nombre usado para almacenar en localStorage
-    const productosCarrito = JSON.parse(localStorage.getItem(carritoStorage));
-
-    if (!productosCarrito || productosCarrito.length === 0) {
-        Swal.fire({
-            title: 'Carrito vacío',
-            text: 'No hay productos en el carrito',
-            icon: 'warning',
-            width: 250,
-            position: 'top-end',
-            confirmButtonText: 'Cerrar'
-        });
-        return;
-    }
-
-    const carritoHTML = productosCarrito.map(producto => `
-        <div class="producto-carrito">
-            <img src="${producto.img}" class="miniatura swal2-image">
-            <p>${producto.marca}</p>
-            <p>Precio: ${producto.precio}</p>
-            <button> - </button>     ${producto.cantidad}     <button> + </button>
-        </div>
-    `).join("");
-
-    Swal.fire({
-        title: 'Carrito',
-        html: carritoHTML,
-        position: 'top-end',
-        width: 250,
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-  
-        imageHeight: 50 // Define la altura de la imagen (opcional)
-    });
-
-   
-
-}
-
-
--- javascript/old.js
+++ javascript/old.js
@@ -1,64 +1,149 @@
-class Producto {
-    constructor(codigo, precio, marca,img) {
+
+
+class Producto {                                //clase Producto
+    constructor(codigo, precio, marca, img) {
         this.codigo = codigo;
         this.precio = precio;
         this.marca = marca;
-        this.img=img;
+        this.img = img;
     }
 }
 
-class listaProductos{
-    constructor(){
-        this.items=[]
+class Lista {                                   //clase Lista para HTML
+    constructor() {
+        this.items = [];
     }
-    agregar(producto){
-        this.items.push(producto)
+    agregar(p) {
+        this.items.push(p);
+    }
+
+    mostrarLista() {                            //Posiciona los objetos en el HTML
+        let contenedor = document.getElementById("contenedor");
+        this.items.forEach(producto => {
+            const tarjetaProducto = document.createElement("div");
+            tarjetaProducto.classList.add("producto");
+            tarjetaProducto.innerHTML = `
+                <img src="${producto.img}">
+                <h2>Precio: ${producto.precio}</h2>
+                <p>Código: ${producto.codigo}</p>
+                <p>Marca: ${producto.marca}</p>
+                <button id="btn-${producto.codigo}"><img src="./img/Shopping-Cart.png"></button>
+            `;
+            contenedor.appendChild(tarjetaProducto);
+        });
     }
 }
-// ... (definiciones de Producto y listaProductos)
 
-const p1 = new Producto("M023", 52412, "RedDragon", "./img/productos/t01.png");
-const p2 = new Producto("M024", 62412, "BlueFalcon", "./img/productos/a01.png");
-const p3 = new Producto("M025", 72412, "GreenPhoenix", "./img/productos/m01.png");
-const p4 = new Producto("M023", 52412, "RedDragon", "./img/productos/t01.png");
-const p5 = new Producto("M024", 62412, "BlueFalcon", "./img/productos/a01.png");
-const p6 = new Producto("M025", 72412, "GreenPhoenix", "./img/productos/m01.png");
+const p1 = new Producto("M023", 32412, "RedDragon", "./img/productos/m01.png");
+const p2 = new Producto("M024", 42412, "BlueFalcon", "./img/productos/m02.png");
+const p3 = new Producto("M025", 52412, "GreenPhoenix", "./img/productos/m01.png");
+const p4 = new Producto("M026", 62412, "RedDragon", "./img/productos/t02.png");
+const p5 = new Producto("M027", 72412, "BlueFalcon", "./img/productos/a01.png");
+const p6 = new Producto("M028", 82412, "GreenPhoenix", "./img/productos/mic02.png");
 
-const l = new listaProductos();
+const listaProductos = new Lista();
 
-l.agregar(p1)
-l.agregar(p2)
-l.agregar(p3)
-l.agregar(p4)
-l.agregar(p4)
-l.agregar(p5)
+listaProductos.agregar(p1);
+listaProductos.agregar(p2);
+listaProductos.agregar(p3);
+listaProductos.agregar(p4);
+listaProductos.agregar(p5);
+listaProductos.agregar(p6);
 
-const contenedor = document.getElementById("contenedor");
+listaProductos.mostrarLista();
 
-l.items.forEach(p => {
-    contenedor.innerHTML += `
-        <div class="producto">
-            <img src="${p.img}">
-            <h2>Precio: ${p.precio}</h2>
-            <p>Código: ${p.codigo}</p>
-            <p>Marca: ${p.marca}</p>
-            <button id="addCarrito">enviar al Carrito</button>
-        </div>
-    `;
+listaProductos.items.forEach(producto => {
+    const btn = document.getElementById(`btn-${producto.codigo}`);
+
+    btn.addEventListener("click", () => agregarCarrito(producto));
 });
 
 
-let addCarrito  = document.getElementById("addCarrito") //capto el objeto y lo almaceno
 
-addCarrito.addEventListener("click",respuesta)          // lo dejo a la escucha
+function agregarCarrito(producto) {
+    console.log("*******" + producto.codigo);
+    let carritoStorage = "carrito"; // Cambia esto a tu nombre de almacenamiento correcto
+    let productosCarrito = JSON.parse(localStorage.getItem(carritoStorage));
+    console.log(productosCarrito);
 
-function respuesta(){
-    console.log("apreto el botonito")                   //respuesta al apretar el boton
-    localStorage.setItem(`valor`,4)                     //coloco el elemento valor en memoria
-                
-    let valor= parseInt(localStorage.getItem(`valor`))            //capto el elemento valor
-    valor=valor+4  
-    console.log(valor)                                  //muestro por consola
+    if (!productosCarrito) {
+        console.log("carrito Vacio");
+        let nuevoProducto = producto;
+        nuevoProducto.cantidad = 1;
+        localStorage.setItem(carritoStorage, JSON.stringify([nuevoProducto]));
+        toasty(producto);
+    } else {
+        console.log("carrito con elementos");
+        let indice = productosCarrito.findIndex(pCarrito => pCarrito.codigo === producto.codigo);
+        console.log(indice);
+        const nuevoproductosCarrito = [...productosCarrito];
+
+        if (indice === -1) {
+            let nuevoProducto = producto;
+            nuevoProducto.cantidad = 1;
+            nuevoproductosCarrito.push(nuevoProducto);
+        } else {
+            nuevoproductosCarrito[indice].cantidad++;
+        }
+        localStorage.setItem(carritoStorage, JSON.stringify(nuevoproductosCarrito));
+        toasty(producto);
+    }
+}
+
+function toasty(producto) {
+    Toastify({
+        text: `${producto.marca} \n Agregado al Carrito`,
+        duration: 2000,
+        style: {
+            background: "linear-gradient(to right, #420885, #313130 )",
+          },
+    }).showToast();
+}
+
+
+/**********************************************/
+
+document.getElementById("mostrar-carrito-btn").addEventListener("click", mostrarCarrito);
+
+function mostrarCarrito() {
+    const carritoStorage = "carrito"; // Asegúrate de que coincida con el nombre usado para almacenar en localStorage
+    const productosCarrito = JSON.parse(localStorage.getItem(carritoStorage));
+
+    if (!productosCarrito || productosCarrito.length === 0) {
+        Swal.fire({
+            title: 'Carrito vacío',
+            text: 'No hay productos en el carrito',
+            icon: 'warning',
+            width: 250,
+            position: 'top-end',
+            confirmButtonText: 'Cerrar'
+        });
+        return;
+    }
+
+    const carritoHTML = productosCarrito.map(producto => `
+        <div class="producto-carrito">
+            <img src="${producto.img}" class="miniatura swal2-image">
+            <p>${producto.marca}</p>
+            <p>Precio: ${producto.precio}</p>
+            <button> - </button>     ${producto.cantidad}     <button> + </button>
+        </div>
+    `).join("");
+
+    Swal.fire({
+        title: 'Carrito',
+        html: carritoHTML,
+        position: 'top-end',
+        width: 250,
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+  
+        imageHeight: 50 // Define la altura de la imagen (opcional)
+    });
+
+   
+
 }
 
 
